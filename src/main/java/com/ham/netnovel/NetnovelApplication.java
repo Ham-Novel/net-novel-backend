a4dd85950eb3625ead880d0da702a545dc760663
JSH <skyjs135@gmail.com>
2024-07-12 21:25:40 +0900

    최초빌드

---
+++ src/main/java/com/ham/netnovel/NetnovelApplication.java
@@ -0,0 +1,13 @@
+package com.ham.netnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetnovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetnovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ham/netnovel/NetnovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ham.netnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetnovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
