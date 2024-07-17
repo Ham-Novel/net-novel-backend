b687ca78c05abc54675b76404aa1ab87ecb3e20b
JSH <skyjs135@gmail.com>
2024-07-17 23:40:35 +0900

    docs: 로그인시 유저 인증정보를 만들때 사용하는 DTO 생성 - providerId, role, nickName, gender 멤버변수 포함

---
+++ src/main/java/com/ham/netnovel/member/dto/MemberLoginDto.java
@@ -0,0 +1,26 @@
+package com.ham.netnovel.member.dto;
+
+
+import com.ham.netnovel.member.OAuthProvider;
+import com.ham.netnovel.member.data.Gender;
+import com.ham.netnovel.member.data.MemberRole;
+import lombok.*;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberLoginDto {
+
+    //유저 정보 제공자에서 관리하는 유저 Id값
+    private String providerId;
+    //유저 역할, ADMIN, AUTHOR, READER 3종류
+    private MemberRole role;
+    //유저 닉네임
+    private String nickName;
+    //성별
+    private Gender gender;
+
+
+}
