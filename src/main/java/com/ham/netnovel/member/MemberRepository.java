c3da24db16f9880dc61358d8ccb75a4d4f6ed1d6
JSH <skyjs135@gmail.com>
2024-07-17 23:39:23 +0900

    docs: Member repository 계층 추가 - JpaRepository extends 하여 사용 - findByProviderId: 유저의 providerId값으로 엔티티를 조회하는 메서드, Optional로 반환

---
+++ src/main/java/com/ham/netnovel/member/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.ham.netnovel.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+
+    /**
+     * OAuth 제공자의 유저 Id값으로 유저 레코드를 찾는 메서드
+     * @param providerId 유저의 providerId값
+     * @return Optional 형태로 반환
+     */
+    Optional<Member> findByProviderId(@Param("providerId")String providerId);
+
+
+}
