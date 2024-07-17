75e11e1aee8640adb92d50c02290b7bfc017c8ea
JSH <skyjs135@gmail.com>
2024-07-17 23:42:01 +0900

    docs: OAuth provider(Naver 등)에서 유저 정보를 파싱할때 사용하는 DTO 생성 - providerId, role, nickName, gender 멤버변수 포함

---
+++ src/main/java/com/ham/netnovel/member/dto/MemberOAuthDto.java
@@ -0,0 +1,24 @@
+package com.ham.netnovel.member.dto;
+
+
+import com.ham.netnovel.member.data.Gender;
+import com.ham.netnovel.member.data.MemberRole;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberOAuthDto {
+
+    //인증 제공자에서 지정한 유저 ID값
+    private String providerId;
+
+    //이름(닉네임)
+    private String nickName;
+
+    //역할
+    private MemberRole role;
+
+    private Gender gender;
+
+}
