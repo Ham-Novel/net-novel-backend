3c5b8f1121b1f69b87dfa50db4498f17180a9456
JSH <skyjs135@gmail.com>
2024-07-18 22:50:18 +0900

    docs : OAuth 로그인시 사용될 User class 추가 - OAuth 제공자에서 보낸 유저 정보를 파싱하여 저장 - Authentic 객체 생성시 사용

---
+++ src/main/java/com/ham/netnovel/OAuth/CustomOAuth2User.java
@@ -0,0 +1,57 @@
+package com.ham.netnovel.OAuth;
+
+import com.ham.netnovel.member.data.Gender;
+import com.ham.netnovel.member.data.MemberRole;
+import com.ham.netnovel.member.dto.MemberOAuthDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class CustomOAuth2User implements OAuth2User {
+
+    private final MemberOAuthDto memberOAuthDto;
+
+    public CustomOAuth2User(MemberOAuthDto memberOAuthDto) {
+        this.memberOAuthDto = memberOAuthDto;
+    }
+
+
+
+    //    제공자별로 형식이 다릅니다. 사용금지
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add((GrantedAuthority) () -> String.valueOf(memberOAuthDto.getRole()));
+
+        return collection;
+    }
+
+    //현재 프로젝트에서 ProviderId를 name으로 사용중입니다.
+    @Override
+    public String getName() {
+        return memberOAuthDto.getProviderId();
+    }
+
+
+    public String getNickName(){
+        return memberOAuthDto.getNickName();
+    }
+
+    public MemberRole getRole(){
+        return memberOAuthDto.getRole();
+    }
+
+    public Gender getGender(){
+        return memberOAuthDto.getGender();
+    }
+}
