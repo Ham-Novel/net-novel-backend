b1c1e9ff1a9eb3658a1d3751cfad14e5cb1b4733
JSH <skyjs135@gmail.com>
2024-07-18 15:22:26 +0900

    docs: 댓글(comment) 리포지토리 계층 생성 - spring DATA JPA extends 하여 사용

---
+++ src/main/java/com/ham/netnovel/comment/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.ham.netnovel.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment,Long> {
+
+
+
+
+
+
+
+}
