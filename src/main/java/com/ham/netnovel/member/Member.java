3ebb66052e7863ebface8b02eaa55767cf8b8394
JSH <skyjs135@gmail.com>
2024-07-18 22:55:03 +0900

    feat : 멤버변수 수정 - MemberRole 컬럼 추가, enum 타입 - Gender 컬럼 추가, enum 타입 유저정보 제공자에서 받는 데이터

--- src/main/java/com/ham/netnovel/member/Member.java
+++ src/main/java/com/ham/netnovel/member/Member.java
@@ -1,9 +1,22 @@
 package com.ham.netnovel.member;
 
 
+import com.ham.netnovel.episodeRating.EpisodeRating;
+import com.ham.netnovel.favoriteNovel.FavoriteNovel;
+import com.ham.netnovel.coinChargeHistory.CoinChargeHistory;
+import com.ham.netnovel.comment.Comment;
+import com.ham.netnovel.member.data.Gender;
+import com.ham.netnovel.member.data.MemberRole;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Member {
 
     @Id
@@ -20,15 +33,50 @@ public class Member {
     //유저 정보 제공자,(naver,kakao등)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Provider provider;
+    private OAuthProvider provider;
 
 
     //유저 정보 제공자에서 관리하는 유저 Id값
     @Column(unique = true)
     private String providerId;
 
+    //유저 역할, ADMIN, AUTHOR, READER 3종류
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
     //유저 닉네임
     private String nickName;
 
+    //성별
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
+
+
+    //보유한 코인의 갯수
+    private Integer coinCount;
+    //junction table 연결, 관심 소설
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<FavoriteNovel> favoriteNovels = new ArrayList<>();
+
+    //junction table 연결,별 점준 에피소드
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<EpisodeRating> episodeRatings = new ArrayList<>();
+
+    //junction table 연결, 코인 충전 기록
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<CoinChargeHistory> coinChargeHistories = new ArrayList<>();
+
+    //junction table 연결, 에피소드 댓글
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    public Member(String email, OAuthProvider provider, String providerId, MemberRole role, String nickName, Gender gender,Integer coinCount) {
+        this.email = email;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.role = role;
+        this.nickName = nickName;
+        this.gender = gender;
+    }
 }
