a74b7b9d8e2b2ddcf9ea5bf4bff51215bbcba5c6
JSH <skyjs135@gmail.com>
2024-07-18 22:51:45 +0900

    docs : OAuth 로그인시 사용될 서비스 계층 추가 - loadUser   - OAuth 제공자(provider) 별로 객체 분리하여 인증정보 생성   - DB에서 유저 정보 조회시, 있을경우 해당 정보로 인증정보 생성, 없을경우 DB에 유저 정보 저장 후 인증정보 생성   - 인증 정보는 CustomOAuth2User 클래스 사용

---
+++ src/main/java/com/ham/netnovel/OAuth/CustomOAuthUserService.java
@@ -0,0 +1,103 @@
+package com.ham.netnovel.OAuth;
+
+import com.ham.netnovel.member.Member;
+import com.ham.netnovel.member.data.Gender;
+import com.ham.netnovel.member.data.MemberRole;
+import com.ham.netnovel.member.Service.MemberService;
+import com.ham.netnovel.member.dto.MemberCreateDto;
+import com.ham.netnovel.member.dto.MemberLoginDto;
+import com.ham.netnovel.member.dto.MemberOAuthDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class CustomOAuthUserService extends DefaultOAuth2UserService {
+
+
+    private final MemberService memberService;
+
+    public CustomOAuthUserService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    /**
+     * OAuth2 제공자에서 보낸 정보 핸들링
+     * @param userRequest
+     * @return
+     * @throws OAuth2AuthenticationException
+     */
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        //oAuth2Response 객체 생성
+        OAuth2Response oAuth2Response = null;
+
+
+        switch (registrationId) {
+            case "naver":
+                log.info("소셜로그인 : naver, 정보={}", oAuth2User.getAttributes());
+
+                oAuth2Response = new NaverOAuthResponse(oAuth2User.getAttributes());
+
+                break;
+
+            case "google":
+                log.info("구글");
+                break;
+
+
+            default:
+                throw new IllegalArgumentException("유효하지 않은 provider");
+        }
+
+
+        //DB에서 유저 정보 조회
+        MemberLoginDto memberLoginInfo = memberService.getMemberLoginInfo(oAuth2Response.getProviderId());
+
+        //반환을 위한 DTO
+        MemberOAuthDto memberOAuthDto;
+
+        //DB에 유저 정보가 없을경우, DB에 유저 정보를 저장
+        if (memberLoginInfo.getProviderId()==null){
+            //OAuth에서 받아온 유저 정보로, 유저 정보 저장을 위한 DTO 생성
+            MemberCreateDto memberCreateDto = MemberCreateDto.builder()
+                    .email(oAuth2Response.getEmail())
+                    .provider(oAuth2Response.getProvider())
+                    .providerId(oAuth2Response.getProviderId())
+                    .nickName(oAuth2Response.getNickName())
+                    .role(MemberRole.READER)
+                    .gender(oAuth2Response.getGender())
+                    .build();
+
+            //유저 정보 생성
+            memberService.createNewMember(memberCreateDto);
+
+            //반환을 위한 DTO 정보 바인딩
+            memberOAuthDto = MemberOAuthDto.builder()
+                    .providerId(oAuth2Response.getProviderId())
+                    .nickName(oAuth2Response.getNickName())
+                    .role(MemberRole.READER)
+                    .gender(oAuth2Response.getGender())
+                    .build();
+        }else {//유저 정보가 DB에 있으면, DB값 바탕으로 DTO 반환(닉네임,role 등 변경 가능성 존재)
+            //반환을 위한 DTO 정보 바인딩
+            memberOAuthDto = MemberOAuthDto.builder()
+                    .providerId(memberLoginInfo.getProviderId())
+                    .nickName(memberLoginInfo.getNickName())
+                    .role(memberLoginInfo.getRole())
+                    .gender(memberLoginInfo.getGender())
+                    .build();
+        }
+
+        return new CustomOAuth2User(memberOAuthDto);
+    }
+}
