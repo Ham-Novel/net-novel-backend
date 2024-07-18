584617fe79a6d8145e70faf38b162628c21744d6
JSH <skyjs135@gmail.com>
2024-07-18 22:48:01 +0900

    feat : 댓글 CRUD 메서드 추가 - getComment   - commentId 값으로 DB에서 엔티티를 찾아 반환하는 메서드   - Optional로 반환, 메서드 사용시 null체크 필요 - updateComment   - 댓글의 내용을 수정하는 메서드   - 수정 요청자와 댓글 작성자가 동일해야 수정   - 수정 요청된 댓글의 episodeId 값과 DB에서 조회된 엔티티의 episodeId값이 같아야 수정   - 조건에 부합하지 않으면 예외로 던짐 - deleteComment   - 댓글의 상태를 삭제상태로 변경하는 메서드   - 삭제 요청자와 댓글 작성자가 동일해야 수정   - 삭제 요청된 댓글의 episodeId 값과 DB에서 조회된 엔티티의 episodeId값이 같아야 수정   - 조건에 부합하지 않으면 예외로 던짐   - 요청이 올바르면 상태를 DELETED_BY_USER 로 변경

--- src/main/java/com/ham/netnovel/comment/service/CommentService.java
+++ src/main/java/com/ham/netnovel/comment/service/CommentService.java
@@ -1,14 +1,41 @@
 package com.ham.netnovel.comment.service;
 
+import com.ham.netnovel.comment.Comment;
 import com.ham.netnovel.comment.dto.CommentCreateDto;
+import com.ham.netnovel.comment.dto.CommentDeleteDto;
+import com.ham.netnovel.comment.dto.CommentUpdateDto;
+
+import java.util.Optional;
 
 public interface CommentService {
 
 
+    /**
+     * commentId 값으로 DB에서 정보를 가져오는 메서드, 사용시 Null체크 필수
+     * @param commentId
+     * @return
+     */
+    Optional<Comment> getComment(Long commentId);
 
-    boolean createComment(CommentCreateDto commentCreateDto);
+    /**
+     * 유저가 작성한 댓글을 DB에 저장하는 메서드
+     *
+     * @param commentCreateDto
+     */
+    void createComment(CommentCreateDto commentCreateDto);
 
-    //조회
 
+    /**
+     * 댓글을 수정하는 메서드
+     * @param commentUpdateDto
+     */
+    void updateComment(CommentUpdateDto commentUpdateDto);
+
+
+    /**
+     * 댓글의 상태를 삭제상태로 바꾸는 메서드
+     * @param commentDeleteDto 삭제 요청된 댓글(comment)의 정보를 담는 DTO
+     */
+    void deleteComment(CommentDeleteDto commentDeleteDto);
 
 }
--- src/main/java/com/ham/netnovel/comment/service/CommentServiceImpl.java
+++ src/main/java/com/ham/netnovel/comment/service/CommentServiceImpl.java
@@ -3,7 +3,10 @@ package com.ham.netnovel.comment.service;
 
 import com.ham.netnovel.comment.Comment;
 import com.ham.netnovel.comment.CommentRepository;
+import com.ham.netnovel.comment.CommentStatus;
 import com.ham.netnovel.comment.dto.CommentCreateDto;
+import com.ham.netnovel.comment.dto.CommentDeleteDto;
+import com.ham.netnovel.comment.dto.CommentUpdateDto;
 import com.ham.netnovel.episode.Episode;
 import com.ham.netnovel.episode.EpisodeService;
 import com.ham.netnovel.member.Member;
@@ -13,6 +16,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -33,11 +37,17 @@ public class CommentServiceImpl implements CommentService {
     }
 
 
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Comment> getComment(Long commentId) {
+        return commentRepository.findById(commentId);
+    }
+
     @Transactional
     @Override
-    public boolean createComment(CommentCreateDto commentCreateDto) {
+    public void createComment(CommentCreateDto commentCreateDto) {
 
-        log.info("댓글정보={}",commentCreateDto.toString());
+        log.info("댓글정보={}", commentCreateDto.toString());
         try {
             //Member 엔티티 조회, null이면 예외로 던짐
             Member member = memberService.getMember(commentCreateDto.getProviderId())
@@ -55,8 +65,6 @@ public class CommentServiceImpl implements CommentService {
             //Comment 엔티티 저장
             commentRepository.save(comment);
 
-            return true;
-
 
         } catch (DataAccessException ex) {
             // 데이터베이스 저장 과정에서 예외 발생 시 처리
@@ -68,4 +76,76 @@ public class CommentServiceImpl implements CommentService {
             throw new RuntimeException("Failed to save comment. Please try again later."); // 예외 던지기
         }
     }
+
+    //ToDo 예외처리 구체적으로 작성
+    @Override
+    @Transactional
+    public void updateComment(CommentUpdateDto commentUpdateDto) {
+        log.info("댓글정보={}", commentUpdateDto.toString());
+
+        try {
+            //null 체크, null이면 예외로 던짐
+            Comment comment = getComment(commentUpdateDto.getCommentId())
+                    .orElseThrow(() -> new RuntimeException("잘못된요청"));
+
+            String providerId = comment.getMember().getProviderId();
+
+            Long episodeId = comment.getEpisode().getId();
+
+
+            //댓글 수정 요청자와 댓글 작성자가 일치하는지 확인
+            //댓글 수정 에피소드와 기존 댓글과 mapping된 에피소드가 일치하는지 확인
+            if (providerId.equals(commentUpdateDto.getProviderId()) && Objects.equals(episodeId, commentUpdateDto.getEpisodeId())) {
+                comment.updateComment(commentUpdateDto.getContent());
+                commentRepository.save(comment);
+
+            } else {
+                throw new RuntimeException("댓글 삭제 에러, 잘못된 유저의 요청");
+            }
+
+        } catch (Exception ex) {
+            //그외 예외처리
+            log.error("Failed to save comment to database: {}", ex.getMessage());
+            throw new RuntimeException("Failed to save comment. Please try again later."); // 예외 던지기
+        }
+
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteComment(CommentDeleteDto commentDeleteDto) {
+
+        try {
+            //null 체크, null이면 예외로 던짐
+            Comment comment = getComment(commentDeleteDto.getCommentId())
+                    .orElseThrow(() -> new RuntimeException("잘못된요청"));
+
+            String providerId = comment.getMember().getProviderId();
+
+            Long episodeId = comment.getEpisode().getId();
+
+            //댓글 삭제 요청자와 댓글 작성자가 일치하는지 확인
+            //댓글 삭제 에피소드와 기존 댓글과 mapping된 에피소드가 일치하는지 확인
+            if (providerId.equals(commentDeleteDto.getProviderId()) && Objects.equals(episodeId, commentDeleteDto.getEpisodeId())) {
+
+                //엔티티의 상태를 삭제 상태로 변경
+                comment.changeStatus(CommentStatus.DELETED_BY_USER);
+
+                //변경된 엔티티를 저장
+                commentRepository.save(comment);
+
+            } else {
+                throw new RuntimeException("에러");
+            }
+
+        } catch (Exception ex) {
+            //그외 예외처리
+            log.error("Failed to delete comment to database: {}", ex.getMessage());
+            throw new RuntimeException("Failed to save comment. Please try again later."); // 예외 던지기
+        }
+
+
+    }
+
 }
