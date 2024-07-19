2bfe3ad3ae4ee186e35461ed54a03ff68ea54701
JSH <skyjs135@gmail.com>
2024-07-19 11:29:35 +0900

    feat: 어노테이션 추가 - @NoArgsConstructor @AllArgsConstructor 추가

--- src/main/java/com/ham/netnovel/member/dto/MemberOAuthDto.java
+++ src/main/java/com/ham/netnovel/member/dto/MemberOAuthDto.java
@@ -3,11 +3,15 @@ package com.ham.netnovel.member.dto;
 
 import com.ham.netnovel.member.data.Gender;
 import com.ham.netnovel.member.data.MemberRole;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberOAuthDto {
 
     //인증 제공자에서 지정한 유저 ID값
