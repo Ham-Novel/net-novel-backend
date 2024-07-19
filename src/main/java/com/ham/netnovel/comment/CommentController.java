20152b81d2eb3e355b0ea61fcafd8e27ecb1b7d4
JSH <skyjs135@gmail.com>
2024-07-19 11:25:50 +0900

    feat: CRUD API 추가 - updateComment   - 댓글의 내용을 변경하는 API - getCommentList   - 에피소드의 댓글을 List로 반환하는 API

--- src/main/java/com/ham/netnovel/comment/CommentController.java
+++ src/main/java/com/ham/netnovel/comment/CommentController.java
@@ -3,7 +3,10 @@ package com.ham.netnovel.comment;
 
 import com.ham.netnovel.OAuth.CustomOAuth2User;
 import com.ham.netnovel.comment.dto.CommentCreateDto;
+import com.ham.netnovel.comment.dto.CommentDeleteDto;
+import com.ham.netnovel.comment.dto.CommentUpdateDto;
 import com.ham.netnovel.comment.service.CommentService;
+import com.ham.netnovel.utils.Authenticator;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +23,13 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    private final Authenticator authenticator;
+
 
     @Autowired
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, Authenticator authenticator) {
         this.commentService = commentService;
+        this.authenticator = authenticator;
     }
 
 
@@ -44,13 +50,8 @@ public class CommentController {
              return ResponseEntity.badRequest().body("에러발생");
         }
 
-        //유저 정보가 없으면 badRequest 응답
-        if (!authentication.isAuthenticated()){
-            return ResponseEntity.badRequest().body("로그인 정보가 없습니다.");
-        }
-
-        //principal 타입캐스팅
-        CustomOAuth2User principal = (CustomOAuth2User) authentication.getPrincipal();
+        //유저 인증 정보가 없으면 badRequest 응답, 정보가 있으면  CustomOAuth2User로 타입캐스팅
+        CustomOAuth2User principal = authenticator.checkAuthenticate(authentication);
 
         //DTO에 유저 정보(providerId) 값 저장
         commentCreateDto.setProviderId(principal.getName());
@@ -59,12 +60,86 @@ public class CommentController {
         commentService.createComment(commentCreateDto);
 
         return ResponseEntity.ok("댓글 추가 완료");
+    }
 
+
+    //댓글 수정 기능
+    @PatchMapping("/comment")
+    public ResponseEntity<String> updateComment(@Valid @RequestBody CommentUpdateDto commentUpdateDto,
+                                                BindingResult bindingResult,
+                                                Authentication authentication){
+
+        //CommentUpdateDto Validation 에러가 있을경우 badRequest 전송
+        if (bindingResult.hasErrors()){
+            log.error("updateComment API 에러발생 ={}",String.valueOf(bindingResult.getFieldError()));
+            return ResponseEntity.badRequest().body("에러발생");
+        }
+
+        //유저 인증 정보가 없으면 badRequest 응답, 정보가 있으면  CustomOAuth2User로 타입캐스팅
+        CustomOAuth2User principal = authenticator.checkAuthenticate(authentication);
+
+        //DTO에 유저 정보(providerId) 값 저장
+        commentUpdateDto.setProviderId(principal.getName());
+
+        commentService.updateComment(commentUpdateDto);
+
+        return ResponseEntity.ok("댓글 수정 완료");
+
+    }
+
+    @DeleteMapping("/comment")
+    public ResponseEntity<String> deleteComment(@Valid @RequestBody CommentDeleteDto commentDeleteDto,
+                                                BindingResult bindingResult,
+                                                Authentication authentication) {
+
+
+        //CommentUpdateDto Validation 에러가 있을경우 badRequest 전송
+        if (bindingResult.hasErrors()){
+            log.error("updateComment API 에러발생 ={}",String.valueOf(bindingResult.getFieldError()));
+            return ResponseEntity.badRequest().body("에러발생");
+        }
+
+        //유저 인증 정보가 없으면 badRequest 응답, 정보가 있으면  CustomOAuth2User로 타입캐스팅
+        CustomOAuth2User principal = authenticator.checkAuthenticate(authentication);
+
+        //DTO에 유저 정보 할당
+        commentDeleteDto.setProviderId(principal.getName());
+
+        //댓글의 상태를 삭제 상태로 변경
+        commentService.deleteComment(commentDeleteDto);
+
+        log.info("댓글 삭제 요청 완료, commentId ={}",commentDeleteDto.getCommentId());
+        return ResponseEntity.ok("삭제완료");
+
+    }
+
+    /**
+     * Episode에 등록된 댓글을 반환하는 API
+     * @param commentDeleteDto
+     * @param bindingResult Bean Validation 에러 정보
+     * @param authentication 유저 인증정보
+     * @return
+     */
+    @GetMapping("/comment")
+    public ResponseEntity<String> getCommentList(@Valid @RequestBody CommentDeleteDto commentDeleteDto,
+                                                BindingResult bindingResult,
+                                                Authentication authentication) {
+
+
+        //유저 인증 정보가 없으면 badRequest 응답, 정보가 있으면  CustomOAuth2User로 타입캐스팅
+        CustomOAuth2User principal = authenticator.checkAuthenticate(authentication);
+
+
+        //episode와 연관된
+        //댓글 전부 가져와서
+        //반환해주세요~
+        return ResponseEntity.ok("ok");
     }
 
 
 
-    //댓글 생성 테스트용 API
+
+        //댓글 생성 테스트용 API
     @GetMapping("/episode/{id}")
     public String commentTest(@PathVariable Integer id){
 
