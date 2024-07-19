3a970dcc7ebf924ed3672b3f1ba2090d38aace04
JSH <skyjs135@gmail.com>
2024-07-19 11:27:51 +0900

    feat: Read 메서드 추가 - findByEpisodeId   - episode의 PK 값으로 엔티티를 찾아 반환하는 메서드

--- src/main/java/com/ham/netnovel/comment/CommentRepository.java
+++ src/main/java/com/ham/netnovel/comment/CommentRepository.java
@@ -1,13 +1,15 @@
 package com.ham.netnovel.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
 
-
-
-
-
-
+    @Query("select c from Comment c " +
+            "where c.episode.id =:episodeId")
+    List<Comment> findByEpisodeId(@Param("episodeId")Long episodeId);
 }
