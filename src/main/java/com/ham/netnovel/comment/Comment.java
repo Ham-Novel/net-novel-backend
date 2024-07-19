d392c0606dba90cb049c318fe8ab3ba6a5ec49ff
JSH <skyjs135@gmail.com>
2024-07-19 11:23:05 +0900

    feat : 생성자, 메서드 추가 - 생성자 추가 content,episode,member를 파라미터로 받는 생성자 - updateComment   - 댓글 엔티티 내용(content)을 바꾸는 메서드 - changeStatus   - 댓글 엔티티의 상태(status)를 바꾸는 메서드

--- src/main/java/com/ham/netnovel/comment/Comment.java
+++ src/main/java/com/ham/netnovel/comment/Comment.java
@@ -4,6 +4,10 @@ package com.ham.netnovel.comment;
 import com.ham.netnovel.episode.Episode;
 import com.ham.netnovel.member.Member;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,17 +17,33 @@ import java.time.LocalDateTime;
  * 에피소드별 댓글 엔티티
  */
 @Entity
+@NoArgsConstructor
+@Getter
 public class Comment {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long commentId;
+    private Long id;
 
 
-    //댓글 내용
-    @Column(nullable = false)
+    //댓글 내용 300자 제한이나, DB 유연성을 위해 305자로 설정
+    @Column(nullable = false,length = 305)
     private String content;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private  CommentStatus status = CommentStatus.ACTIVE;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
 
 
     //N인 comment에서 외래키 가져감
@@ -37,12 +57,25 @@ public class Comment {
     private Member member;
 
 
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 
 
+
+    @Builder
+    public Comment(String content, Episode episode, Member member) {
+        this.content =content;
+        this.episode = episode;
+        this.member = member;
+
+    }
+
+    //댓글 엔티티 내용 변경
+    public void updateComment(String content){
+        this.content = content;
+    }
+
+    //댓글 엔티티 상태 변경
+   public  void changeStatus(CommentStatus status) {
+        this.status = status;
+    }
 
 }
