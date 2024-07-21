2d313be8730ead9a5c3371463daf81c6bc009fb0
JSH <skyjs135@gmail.com>
2024-07-21 20:45:21 +0900

    docs: 대댓글(ReComment) 엔티티 계층 추가 - 컬럼으로 id(PK), content(내용), status(삭제,게시 등 상태 enum타입), createdAt, updatedAt 포함 - Comment, Member 엔티티와 다대일 관계

---
+++ src/main/java/com/ham/netnovel/reComment/ReComment.java
@@ -0,0 +1,71 @@
+package com.ham.netnovel.reComment;
+
+
+import com.ham.netnovel.comment.Comment;
+import com.ham.netnovel.comment.CommentStatus;
+import com.ham.netnovel.member.Member;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ReComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    //대댓글 내용 300자 제한이나, DB 유연성을 위해 305자로 설정
+    @Column(nullable = false,length = 305)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CommentStatus status = CommentStatus.ACTIVE;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    //N인 reComment에서 외래키 가져감
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    //작성한 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+    @Builder
+    public ReComment(String content, Comment comment, Member member) {
+        this.content = content;
+        this.comment = comment;
+        this.member = member;
+    }
+
+    //대댓글 엔티티 내용 변경
+    public void updateReComment(String content){
+        this.content = content;
+    }
+
+    //대댓글 엔티티 상태 변경
+    public  void changeReStatus(CommentStatus status) {
+        this.status = status;
+    }
+
+
+
+}
