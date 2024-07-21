d7bb97522afeacf639d93d9f67ad7d56a5be53ff
JSH <skyjs135@gmail.com>
2024-07-21 20:49:43 +0900

    docs: 대댓글(ReComment) 리포지토리 계층 추가 - findByCommentId   - commentId(댓글의PK) 값으로, 댓글에 달린 대댓글 엔티티를 list로 반환하는 메서드

---
+++ src/main/java/com/ham/netnovel/reComment/ReCommentRepository.java
@@ -0,0 +1,16 @@
+package com.ham.netnovel.reComment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReCommentRepository extends JpaRepository<ReComment, Long> {
+
+
+    @Query("select r from ReComment r " +
+            "where r.comment.id = :commentId")
+    List<ReComment> findByCommentId(@Param("commentId")Long commentId);
+
+}
