45c75e8dab841771bf150ab32eb83035230a824f
JSH <skyjs135@gmail.com>
2024-07-21 20:53:58 +0900

    docs: ReCommentUpdateDto, 대댓글 내용 업데이트용 DTO 추가 - content(내용), commentId(댓글의PK), reCommentId(대댓글의PK) 멤버변수로 가짐

---
+++ src/main/java/com/ham/netnovel/reComment/dto/ReCommentUpdateDto.java
@@ -0,0 +1,34 @@
+package com.ham.netnovel.reComment.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
+@Setter
+public class ReCommentUpdateDto {
+
+    @NotBlank
+    @Size(max = 300,message = "댓글은 300자 이하로 작성해주세요!")
+    private String content;
+
+
+    //댓글의 Id값
+    @NotNull
+    private Long commentId;
+
+    //대댓글 id값
+    @NotNull
+    private Long reCommentId;
+
+
+    private String providerId;
+
+}
