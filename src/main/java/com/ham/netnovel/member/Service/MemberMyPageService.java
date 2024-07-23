a4f0ba7500be6d70a489f9b5160095fffe8c3e21
JSH <skyjs135@gmail.com>
2024-07-23 12:28:07 +0900

    docs : MemberMyPageService, 마이페이지에 사용될 메서드를 담는 서비스 계층 추가 - 기존 MemberService CommentService와 순환참조 문제로 새로운 서비스계층 추가 - getMemberCommentAndReCommentList   - providerId값으로 유저가 작성한 댓글과 대댓글을 반환하는 메서드   - ReCommentService, CommentService 계층의 메서드 사용   - 댓글과 대댓글은 MemberCommentDto로 변환하여 반환   - MemberCommentDto 의 멤버변수 type 으로 댓글/대댓글 타입 지정

---
+++ src/main/java/com/ham/netnovel/member/Service/MemberMyPageService.java
@@ -0,0 +1,15 @@
+package com.ham.netnovel.member.Service;
+
+
+import com.ham.netnovel.member.dto.MemberCommentDto;
+
+import java.util.List;
+
+public interface MemberMyPageService {
+
+
+    List<MemberCommentDto> getMemberCommentAndReCommentList(String providerId);
+
+
+
+}
