a136d9b05dde655f9774823856ef91c95ef9a2d4
JSH <skyjs135@gmail.com>
2024-07-25 23:37:17 +0900

    - feat favoriteNovel 패키지 이름 변경   - FavoriteNovel-> favoriteNovel

--- src/main/java/com/ham/netnovel/FavoriteNovel/FavoriteNovel.java
+++ src/main/java/com/ham/netnovel/FavoriteNovel/FavoriteNovel.java
@@ -1,27 +1,25 @@
-package com.ham.netnovel.FavoriteNovel;
-
+package com.ham.netnovel.favoriteNovel;
 
 import com.ham.netnovel.member.Member;
 import com.ham.netnovel.novel.Novel;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class FavoriteNovel {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//auto_increment 자동생성
     private Long id;
-
-
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
-
-
     @ManyToOne
     @JoinColumn(name = "novel_id")
     private Novel novel;
+
 
 
 }
