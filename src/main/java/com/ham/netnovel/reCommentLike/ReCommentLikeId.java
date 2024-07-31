513e5f1f08390e51eebbb17368ecfd594087a9f8
JSH <skyjs135@gmail.com>
2024-07-31 12:05:50 +0900

    - refactor : EmbeddedId 정의 클래스, 클래스명 통일(key->id)
    - CommentLikeId
      - CommentLikeKey 에서 클래스명 변경
    
    - CommentLike,CommentLikeRepository, CommentLikeServiceImpl
      - CommentLikeId 클래스명 변경에 따른 import, 객체명 수정
    
    - ReCommentLikeId
      - ReCommentLikeKey 에서 클래스명 변경
    
    - ReCommentLike, ReCommentLikeRepository , ReCommentLikeServiceImpl
      - ReCommentLikeKey 클래스명 변경에 따른 import, 객체명 수정

--- src/main/java/com/ham/netnovel/reCommentLike/ReCommentLikeId.java
+++ src/main/java/com/ham/netnovel/reCommentLike/ReCommentLikeId.java
@@ -1,4 +1,4 @@
-package com.ham.netnovel.commentLike;
+package com.ham.netnovel.reCommentLike;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
@@ -14,25 +14,26 @@ import java.util.Objects;
 @AllArgsConstructor
 @Getter
 @Setter
-public class CommentLikeKey implements Serializable {//composite PK 정의 class
-    private Long commentId;
+public class ReCommentLikeId implements Serializable {
+
+    private Long reCommentId;
     private Long memberId;
 
-    //객체 동등성 판단
+
     @Override
     public boolean equals(Object o) {
         //파라미터로 받은 객체와 자신을 비교, 동일한 객체일경우 true 반환
-        if (this == o) return true;
+        if (this==o) return true;
         //파라미터로 받은 객체가 null 이거나, 현재 클래스와 다른 클래스일경우 false 반환
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o==null || getClass() !=o.getClass()) return false;
         //타입 캐스팅
-        CommentLikeKey that = (CommentLikeKey) o;
+        ReCommentLikeId that = (ReCommentLikeId) o;
         //파라미터로 받은 객체와 현재 객체의 필드값 비교, 두 필드값이 모두 동일해야 true 반환
-        return Objects.equals(commentId, that.commentId) && Objects.equals(memberId, that.memberId);    }
-
+        return Objects.equals(reCommentId,that.reCommentId) && Objects.equals(memberId,that.memberId);
+    }
     //equals()가 true를 반환할때, 객체들이 도일한 해시 코드 값을 반환하도록 보장하는 메서드
     @Override
     public int hashCode() {
-        return Objects.hash(commentId, memberId);
+        return Objects.hash(reCommentId, memberId);
     }
 }
