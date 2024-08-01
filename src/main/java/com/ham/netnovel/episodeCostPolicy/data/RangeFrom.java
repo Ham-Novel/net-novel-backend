77d78f263b3ab81438b537d9b0740ad20b41d3b8
ned-razzz <smg9972@gmail.com>
2024-08-01 23:37:26 +0900

    feat: episodeCostPolicy 엔티티 생성
    - 에피소드 타입별 가격을 담당하는 엔티티
    - 제약조건 데이터를 더해 잘못된 가격 설정 방치.

---
+++ src/main/java/com/ham/netnovel/episodeCostPolicy/data/RangeFrom.java
@@ -0,0 +1,6 @@
+package com.ham.netnovel.episodeCostPolicy.data;
+
+public enum RangeFrom {
+    RECENT, //최신 회차부터
+    INITIAL
+}
---
+++ src/main/java/com/ham/netnovel/episodeCostPolicy/episodeCostPolicy.java
@@ -0,0 +1,15 @@
+package com.ham.netnovel.episodeCostPolicy;
+
+import com.ham.netnovel.episodeCostPolicy.data.RangeFrom;
+
+public class episodeCostPolicy {
+    private Long id;
+
+    private String name;
+
+    private Integer coinCost;
+
+    private RangeFrom rangeFrom;
+
+    private Integer rangeValue;
+}
