866fb9cd9b96d0f475ccee652671b046ae1ce076
JSH <skyjs135@gmail.com>
2024-08-01 13:28:50 +0900

    - feat : 스케줄링을 위한 quartz 라이브러리 추가, Novel 평균 점수 계산 스케줄러 등록
    - build.gradle
      - quartz 라이브러리 의존관계 추가
    
    - NovelAverageRatingJob
      - 소설 평균 점수 계산 로직을 스케줄러에 등록하기 위한 Job 클래스 추가
      - 현재 로직 주석처리, 배포시 사용
    
    - QuartzConfig
      - quartz 라이브러리 스케줄 정의를 위한 config 클래스 추가
      - 소설 평균점수 계산을 위한 JobDetail 메서드 추가(novelAverageRatingJobDetail)
      - 소설 평균점수 계산 트리거 추가 (novelAverageRatingTrigger)
        - 실행주기 12시간 설정

---
+++ src/main/java/com/ham/netnovel/novelAverageRating/NovelAverageRatingJob.java
@@ -0,0 +1,28 @@
+package com.ham.netnovel.novelAverageRating;
+
+import com.ham.netnovel.novelAverageRating.service.NovelAverageRatingService;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class NovelAverageRatingJob implements Job {
+
+
+
+    private final NovelAverageRatingService novelAverageRatingService;
+    @Autowired
+    public NovelAverageRatingJob(NovelAverageRatingService novelAverageRatingService) {
+        this.novelAverageRatingService = novelAverageRatingService;
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+//        log.info("Novel 평균 별점 업데이트 시작");
+//        novelAverageRatingService.updateAverageRatingForAllRatedNovels();
+    }
+}
