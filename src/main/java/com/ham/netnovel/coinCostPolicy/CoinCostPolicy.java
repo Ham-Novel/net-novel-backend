bf49438ffd02e185546687255502bc25402826c0
ned-razzz <smg9972@gmail.com>
2024-08-02 18:39:10 +0900

    form: CoinCostPolicy 엔티티 프로퍼티 주석 추가

--- src/main/java/com/ham/netnovel/coinCostPolicy/CoinCostPolicy.java
+++ src/main/java/com/ham/netnovel/coinCostPolicy/CoinCostPolicy.java
@@ -17,12 +17,15 @@ public class CoinCostPolicy {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment 자동 생성
     private Long id;
 
+    //정책명
     @Column(unique = true)
     private String name;
 
+    //정책 가격
     @Column(nullable = false)
     private Integer coinCost;
 
+    //해당 정책을 쓰는 에피소드 리스트
     @OneToMany(mappedBy = "coinCostPolicy")
     private List<Episode> episodes;
 
