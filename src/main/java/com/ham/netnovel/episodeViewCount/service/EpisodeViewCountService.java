0b4ca80176dfd21b4b99f5bd941df803e5eb5312
JSH <skyjs135@gmail.com>
2024-08-16 00:10:00 +0900

    feat : 랭킹 집계방식 변경에 따른, EpisodeViewCount 도메인 로직 수정 - 항목별 가중치 생성, 조회수를 가중치에 따라 계산하여 반환하도록 수정
    - EpisodeViewCountRepository
      - findWeeklyNovelTotalViews
        - 메서드 삭제, findNovelTotalViews 생성하여 역할 대체
    
      - findNovelTotalViews
        - 특정 기간에 대한 조회수를 소설별로 그룹화하여 반환하는 메서드 추가
        - 파라미터로 startDate(집계 시작 날짜), endDate(집계 마지막 날짜) 를 받음
        - 집계 기간동안 소설의 조회수를 날짜별로 그룹화하여 반환
        - 배열의 첫번째 요소는 Novel 엔티티, 두번째는 총 조회수, 세번째는 조회수가 기록된 날짜를 담아 반환
    
    - EpisodeViewCountService
      - getWeeklyRanking
        - 사용하지 않는 메서드, 삭제
    
      - 가중치 상수 정의
        - 오늘 조회수 가중치 3, 어제 조회수 가중치 2 으로 설정
    
      - getDailyRanking
         - 단순히 조회수로 랭킹을 정하는것이 아닌, 항목마다 가중치를 두어 합산 점수로 랭킹을 정하도록 로직 변경
         - 메서드 실행일 기준 어제 조회수는 1당 2점, 오늘 조회수는 1당 3점으로 가중치 설정
         - ToDo 소설별점, 댓글 가중치 추가
    
    - EpisodeViewCount
      - episode_id 와 view_date 를 composite key 로 설정하여 중복 데이터 방지 (유니크 제약조건)

--- src/main/java/com/ham/netnovel/episodeViewCount/service/EpisodeViewCountService.java
+++ src/main/java/com/ham/netnovel/episodeViewCount/service/EpisodeViewCountService.java
@@ -18,22 +18,12 @@ public interface EpisodeViewCountService {
     void updateEpisodeViewCountEntity(Map<Long, Episode> episodes, List<ViewCountIncreaseDto> viewCountIncreaseDtos);
 
 
-
     /**
      * 특정 날짜의 소설 조회수로 랭킹을 계산해 반환하는 메서드
      * @param todayDate 계산할 날짜
      * @return List Novel 엔티티와 totalViews(총조회수) 를 DTO에 담아 List 형태로 반환
      */
     List<NovelRankingUpdateDto> getDailyRanking(LocalDate todayDate);
-
-    /**
-     * 한주동안의 소설 조회수로 랭킹을 계산해 반환하는 메서드
-     * 파라미터로 입력되는 날짜 기준으로 7일전 ~ 1일전 까지의 조회수 계산
-     * @param todayDate 계산할 날짜
-     * @return List Novel 엔티티와 totalViews(총조회수) 를 DTO에 담아 List 형태로 반환
-     */
-    List<NovelRankingUpdateDto>  getWeeklyRanking(LocalDate todayDate);
-
 
     /**
      * 특정 에피소드의 조회수를 1 올리는 메서드, 데이터는 Redis 에 저장
