9679f580bc91e9933bab2a182b8ffd6fc1f093a2
JSH <skyjs135@gmail.com>
2024-09-07 00:46:10 +0900

    feat: ValidationErrorHandler 클래스 에러 핸들링 메서드 추가
    - ValidationErrorHandler
      - handleValidationErrorMessages
        - bindingResult  와 methodName 을 받아 에러를 처리하는 메서드 추가
        - bindingResult 을 List로 반환하고, methodName 으로 log를 남김

--- src/main/java/com/ham/netnovel/common/utils/ValidationErrorHandler.java
+++ src/main/java/com/ham/netnovel/common/utils/ValidationErrorHandler.java
@@ -1,11 +1,14 @@
 package com.ham.netnovel.common.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
 import java.util.List;
 
 
+@Slf4j
 public class ValidationErrorHandler {
 
     //static으로 인스턴스화 하지 않고 사용
@@ -14,5 +17,17 @@ public class ValidationErrorHandler {
            return bindingResult.getFieldErrors().stream()
                     .map(FieldError::getDefaultMessage)
                     .toList();
+    }
+
+
+    public static List<String> handleValidationErrorMessages(BindingResult bindingResult,String methodName ) {
+
+        //에러 메시지 List로 추출
+        List<String> errorMessages = bindingResult.getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+        log.error("{} }API 에러발생={}", methodName,errorMessages);
+        //에러 메시지 반환
+        return errorMessages;
     }
 }
