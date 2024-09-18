2053d880e72d89038997cf37d82ea81c8048f118
ned-razzz <smg9972@gmail.com>
2024-09-18 19:20:21 +0900

    feat: Novel 생성 api tag 생성 로직 추가

--- src/main/java/com/ham/netnovel/novel/dto/NovelCreateDto.java
+++ src/main/java/com/ham/netnovel/novel/dto/NovelCreateDto.java
@@ -1,9 +1,10 @@
 package com.ham.netnovel.novel.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,6 +19,8 @@ public class NovelCreateDto {
 
     @Size(max = 300)
     private String description;
+
+    private List<String> tagNames; //작품 태그
 
     private String accessorProviderId; //실행자 유저 ID
 }
