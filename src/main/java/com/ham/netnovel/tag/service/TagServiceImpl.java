be1ca4a366780fa38c2b6aa9d5fcbe8ea37f6ede
JSH <skyjs135@gmail.com>
2024-09-26 11:48:24 +0900

    feat: 태그 생성 로직 수정 및 검증 추가
    - String 타입의 태그명을 받아 처리하도록 변경
    - 태그명을 받아 DB에서 조회하거나, 없을 경우 새로 생성하는 로직 추가
    
    ### TagService/TagServiceImpl
    - createTag
      - 파라미터를 DTO에서 String 타입의 tagName으로 변경
      - 반환 타입을 Long에서 Tag 타입으로 변경 (생성된 태그 엔티티 객체 반환)
    
    - getOrCreateTag
      - 태그명을 받아 해당 태그 엔티티 객체를 반환하는 메서드 추가
      - DB에서 태그를 조회해 반환하고, 없을 경우 createTag 메서드를 호출해 태그를 생성한 후 반환
    
    ### TagServiceImpl
    - validateTagName
      - 태그명을 검증하는 메서드 추가
      - 태그명이 null, 비어있거나, 앞뒤 공백 제거 후 10자 이상이거나, 영어/한글/숫자 외의 문자가 포함된 경우 예외 발생
      - 문제가 없을 경우, 앞뒤 공백을 제거한 태그명을 반환

--- src/main/java/com/ham/netnovel/tag/service/TagServiceImpl.java
+++ src/main/java/com/ham/netnovel/tag/service/TagServiceImpl.java
@@ -4,8 +4,8 @@ import com.ham.netnovel.common.exception.ServiceMethodException;
 import com.ham.netnovel.tag.Tag;
 import com.ham.netnovel.tag.TagRepository;
 import com.ham.netnovel.tag.TagStatus;
-import com.ham.netnovel.tag.dto.TagCreateDto;
 import com.ham.netnovel.tag.dto.TagDeleteDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,14 +14,15 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
-public class TagServiceImpl implements TagService{
+@Slf4j
+public class TagServiceImpl implements TagService {
 
-    TagRepository tagRepository;
+   private final TagRepository tagRepository;
 
-    @Autowired
     public TagServiceImpl(TagRepository tagRepository) {
         this.tagRepository = tagRepository;
     }
+
 
     @Override
     @Transactional(readOnly = true)
@@ -37,21 +38,88 @@ public class TagServiceImpl implements TagService{
 
     @Override
     @Transactional
-    public Long createTag(TagCreateDto createDto) {
-        try {
-            if (tagRepository.existsByName(createDto.getName())) {
-                throw new RuntimeException("이미 존재하는 값입니다.");
-            }
+    public Tag createTag(String tagName) {
 
-            Tag newRecord = Tag.builder()
-                    .name(createDto.getName())
+        //파라미터 검증, 태그가 null 이거나 비어있으면 안되고, 10글자 이하의 한글/숫자/영문 이여야함
+        String validateTagName = validateTagName(tagName);
+
+        //태그명이 존재하는 경우 예외로 던짐
+        if (tagRepository.existsByName(validateTagName)) {
+            throw new ServiceMethodException("createTag 에러, 동일한 태그명이 존재합니다. 태그명={}" + tagName);
+        }
+
+        try {
+            //새로운 tag 엔티티 객체 생성
+            Tag tag = Tag.builder()
+                    .name(validateTagName)
                     .status(TagStatus.ACTIVE)
                     .build();
-            Tag saved = tagRepository.save(newRecord);
-            return saved.getId();
+
+            //만들어진 Tag 엔티티 반환
+
+            Tag save = tagRepository.save(tag);
+            log.info("새로운 Tag 생성 완료, 태그명 ={} ",save.getId());
+            return save;
+
         } catch (Exception ex) {
             throw new ServiceMethodException("createTag() Error : " + ex.getMessage());
         }
+    }
+
+    @Override
+    @Transactional
+    public Tag getOrCreateTag(String tagName) {
+        try {
+            //태그 엔티티를 가져와서 반환하거나,
+            // DB에 태그 엔티티가 없으면 새로만들어서 반환
+            Optional<Tag> tagByName = getTagByName(tagName);
+
+            return tagByName.orElseGet(() -> createTag(tagName));
+        } catch (Exception ex) {
+            throw new ServiceMethodException("getOrCreateTag 에러 : " + ex + ex.getMessage());
+        }
+
+    }
+
+    /**
+     * 태그명의 유효성을 검사하는 메서드 입니다.
+     *
+     * <p>
+     * 태그명이 null 이거나 객체가 비어있는 경우 예외로 던집니다.
+     * </p>
+     *
+     * <p>
+     * 태그명 앞뒤 공백을 제거한후 10자 이상이거나, 영어/한글/숫자 외의 문자가
+     * 포함된경우 예외로 던집니다.
+     * </p>
+     *
+     * @param tagName 태그명
+     * @return 앞뒤 공백을 제거한 태그명 {@link String} 객체
+     */
+    private String validateTagName(String tagName) {
+
+        //파라미터 null, 비어있는지 체크
+        if (tagName == null || tagName.isEmpty()) {
+            throw new IllegalArgumentException("validateTagName 메서드 에러, 파라미터가 null 이거나 비었습니다.");
+        }
+
+        //비어있지 않으면, 앞뒤 공백 제거한 객체 생성
+        String trimmedName = tagName.trim();
+
+        //앞뒤 공백 제거한 길이가 10자리 이상이면 예외로 던짐
+        if (trimmedName.length() > 10) {
+            throw new IllegalArgumentException("validateTagName 메서드 에러, 파라미터가 10자를 넘어갑니다. tagName= " + tagName);
+        }
+
+        // 영어, 숫자, 한글, 중간 공백만 허용하는 정규식,
+        if (!trimmedName.matches("[a-zA-Z0-9가-힣 ]+")) {
+            throw new IllegalArgumentException("validateTagName 메서드 에러, 영어, 숫자, 한글 및 중간 공백만 허용됩니다. tagName= " + tagName);
+        }
+
+        //이상 없으면 앞뒤공백 자른 태그이름 반환
+        return trimmedName;
+
+
     }
 
     @Override
