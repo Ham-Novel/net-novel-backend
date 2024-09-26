f85e3856d6139f560b328fe16b55ed3d151e327f
JSH <skyjs135@gmail.com>
2024-09-26 17:30:11 +0900

    feat: Tag 검색 로직 추가
    ### TagRepository
    - findBySearchWord
      - 검색어를 바탕으로 태그 이름이 부분 일치하는 레코드의 태그 이름을 반환하는 메서드 추가
      - 반환 타입은 `List<String>`.
    
    ### TagService / TagServiceImpl
    - getTagNamesBySearchWord
      - 검색어의 유효성 검사를 수행한 후, 검색어와 부분 일치하는 태그 이름을 반환하는 메서드 추가.
      - SQL injection 공격에 사용되는 특수 문자를 제거한 후 DAO 메서드 호출.
      - 검색어가 10자 이상이거나 비어있으면 빈 리스트를 반환.
    
    ### TagController
    - Tag 컨트롤러 계층 추가:
    
    - getTagNamesBySearchWord:
      - 검색어와 일치하는 태그 이름 목록을 담은 `ResponseEntity` 객체를 반환하는 API 추가.

--- src/main/java/com/ham/netnovel/tag/TagRepository.java
+++ src/main/java/com/ham/netnovel/tag/TagRepository.java
@@ -1,7 +1,10 @@
 package com.ham.netnovel.tag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
@@ -9,6 +12,21 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
     //unique 속성의 name 프로퍼티로 Tag 조회
     Optional<Tag> findByName(String name);
+
+    /**
+     * 주어진 검색어를 기반으로 태그 이름 목록을 조회하는 메서드 입니다.
+     * <p>
+     * 태그 이름이 검색어를 포함하는 경우에 해당하는 태그 이름을 반환합니다.
+     * 검색어는 부분 일치로 검색됩니다.
+     * </p>
+     *
+     * @param searchWord 검색할 태그의 일부 또는 전체 이름
+     * @return 검색어와 일치하는 태그 이름 목록
+     */
+    @Query("select t.name " +
+            "from Tag t " +
+            "where t.name like %:searchWord%")
+    List<String> findBySearchWord(@Param("searchWord")String searchWord);
 
     //unique 속성의 name 값 중복 확인
     boolean existsByName(String name);
