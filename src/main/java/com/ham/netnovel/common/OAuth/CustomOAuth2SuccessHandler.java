51de003cf7f0093735515b1da49519366453fc42
JSH <skyjs135@gmail.com>
2024-12-21 00:09:41 +0900

    feat: 로그인 성공 시 환경변수 기반 도메인 리다이렉션 추가
    - CustomOAuth2SuccessHandler
      - 환경변수 `front.end.domain` 값을 @Value 어노테이션으로 주입받아 사용
      - 로그인 성공 시 해당 도메인으로 리다이렉션 처리

--- src/main/java/com/ham/netnovel/common/OAuth/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/ham/netnovel/common/OAuth/CustomOAuth2SuccessHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -18,19 +19,17 @@ public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHa
     private final RequestCache requestCache = new HttpSessionRequestCache();
 
 
-    //TODO 리다이렉트 URL 도메인으로 변경 필수!!!
+
+    @Value("${front.end.domain}")
+   private String frontEndDomain;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
-//        SavedRequest savedRequest = requestCache.getRequest(request, response);
-
-        String redirectUrl = request.getSession().getAttribute("redirectUrl") != null
-                ? (String) request.getSession().getAttribute("redirectUrl")
-                : "http://localhost:5173/";//
 
         // 로그인 성공 시 세션에 저장된 URL로 리다이렉트
-        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+        getRedirectStrategy().sendRedirect(request, response, frontEndDomain);
+
     }
 }
 
